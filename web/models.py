0e6dbf99d846e8eaa8a28df42c853351134b8283
andres-root <andres.root.coder@gmail.com>
2016-05-22 03:27:06 -0500

    working on orders page

--- web/models.py
+++ web/models.py
@@ -1,3 +1,22 @@
+
 from django.db import models
 
-# Create your models here.
+
+class Preorder(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    robot = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    phone = models.CharField(max_length=200)
+    gender = models.CharField(max_length=200)
+    robotname = models.CharField(max_length=200)
+    shirt = models.BooleanField(default=False)
+    satellite = models.BooleanField(default=False)
+    free = models.BooleanField(default=False)
+    comments = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True, blank=True)
+    date_updated = models.DateTimeField(auto_now=True, blank=True)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return '{0} {1}'.format(self.first_name, self.last_name)
--- web/urls.py
+++ web/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^order/', views.order, name='order'),
     url(r'^$', views.index, name='index'),
+    url(r'^order/', views.order, name='order'),
 ]
