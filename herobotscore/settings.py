fb465f2a59a8d847952915c77e655e7377bfa602
andres-root <andres.root.coder@gmail.com>
2016-05-22 04:20:43 -0500

    working on orders page

--- herobotscore/settings.py
+++ herobotscore/settings.py
@@ -15,7 +15,6 @@ import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
@@ -136,6 +135,7 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/dist/'
 
 STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'dist'),
     os.path.join(PROJECT_ROOT, 'dist'),
 ]
 
